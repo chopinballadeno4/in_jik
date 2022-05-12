edc08fa6cad0daafcd4e3ef5f446dfcb109309a6
happybae <qo98333@naver.com>
2022-05-13 00:27:43 +0900

    header layout

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -3,16 +3,62 @@ import styled from "styled-components";
 
 function Home() {
     const Entire = styled.div`
-    
+        margin: 0;
+        padding: 0;
+        width: 100vw;
+        height: 100vh;
+        display: grid;
+        grid-template-rows: 187px 9fr 1fr;
+    `;
+
+    const Header = styled.header`
+        padding-left: 0px;
+        background-color: #ffffff;
+        grid-row: 0 1;
+        display: flex;
+        flex-direction: column;
+        width: 100vw;
+        height: 100vh;
+    `;
+
+    const HeaderSearch = styled.div`
+        width: 100vw;
+        height: 134px;
+    `;
+
+    const HeaderMenu = styled.div`
+        border-top: thin solid #e5e5e5;
+        border-bottom: 3px solid #ffd203;
+        width: 100vw;
+        height: 50px;
+    `;
+
+    const Main = styled.main`
+        background-color: #f7f7f7;
+        grid-row: 1 2;
+    `;
+
+    const Bottom = styled.article`
+        background-color: orange;
+        grid-row: 2 3;
     `;
 
 
     return (
-        <>
-            <Entire>
-                <h1> home</h1>
-            </Entire>
-        </>
+        <Entire>
+            <Header>
+                <HeaderSearch>
+                </HeaderSearch>
+                <HeaderMenu>
+                </HeaderMenu>
+            </Header>
+            <Main>
+                <h1>Main</h1>
+            </Main>
+            <Bottom>
+                <h1>Bottom</h1>
+            </Bottom>
+        </Entire>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from "styled-components";
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const darkmode = {
+
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkmode}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
