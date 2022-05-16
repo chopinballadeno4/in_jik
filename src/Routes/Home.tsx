bbca5cc21e48e4f2e14cf8036c5a66db7c8a07ab
happybae <qo98333@naver.com>
2022-05-16 12:22:49 +0900

    BottomInfo, BottomContact yey

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -25,7 +25,7 @@ function Home() {
         width: 100vw;
         height: 100vh;
         display: grid;
-        grid-template-rows: 187px 9fr 1fr;
+        grid-template-rows: 187px 1200px 250px;
     `;
 
     const Header = styled.header`
@@ -156,9 +156,40 @@ function Home() {
 
     //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Buttom
     const Bottom = styled.article`
-        background-color: orange;
+        background-color: #ffffff;
         grid-row: 2 3;
     `;
+
+    const Bottomnav = styled.div`
+        height: 50px;
+        border-top: 2px solid #1e90ff;
+        border-bottom: thin solid #e5e5e5;
+    `;
+
+    const BottomInfo = styled(UlParent)`
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        li {
+            margin: 0 30px;
+            float: left;
+            button {
+                &:hover {
+                    opacity: 0.5;
+                }
+                span {
+                    font-size: 15px;
+                    color : #222;
+                }
+            }
+        }
+    `;
+
+    const BottomContact = styled.div`
+        
+    `;
+
 
     const toSignIn = () => {
 
@@ -238,7 +269,21 @@ function Home() {
                 <h1>Main</h1>
             </Main>
             <Bottom>
-                <h1>Bottom</h1>
+                <Bottomnav>
+                    <BottomInfo>
+                            <li><button><span>회사소개</span></button></li>
+                            <li><button><span>인재채용</span></button></li>
+                            <li><button><span>회원약관</span></button></li>
+                            <li><button><span style={{fontWeight: "bold"}}>개인정보처리방침</span></button></li>
+                            <li><button><span>이메일무단수집거부</span></button></li>
+                            <li><button><span>채용정보 API</span></button></li>
+                            <li><button><span>제휴문의</span></button></li>
+                            <li><button><span>고객센터</span></button></li>
+                    </BottomInfo>
+                </Bottomnav>
+                <BottomContact>
+
+                </BottomContact>
             </Bottom>
         </Entire>
     );
